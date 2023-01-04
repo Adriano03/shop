34d9f494dc6c8bc06385560018770494467f8467
Adriano Reis <adriano16_gubert@hotmail.com>
2023-01-04 10:50:29 -0300

    v7.1

---
+++ lib/utils/constants_template.dart.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: constant_identifier_names
+
+// Classe modelo para armazenar URLs firebase;
+class ConstantsTemplateDart {
+  static const USER_FAVORITES_URL = 'shop.firebase/userFavorites';
+  static const PRODUCT_BASE_URL = 'shop.firebase/products';
+  static const ORDERS_BASE_URL = 'shop.firebase/orders';
+}
